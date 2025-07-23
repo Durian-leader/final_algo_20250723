7d227d87150600c8620c8e827eeba148442b92de
lidonghao <lidonghao100@outlook.com>
2025-07-23 23:13:36 +0800

    完成512的训练，效果较好

---
+++ SPK/spk-OFFICIAL_ALL-NO_NOISE_40epoch/inc/spk_fc_model_params.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdint.h>
+#include "riscv_nnfunctions.h"
+
+#define SPK_FC_IN_DIM   (256)
+#define SPK_FC_OUT_DIM  (5)
+
+extern const int8_t  spk_fc_weights[];
+extern const int32_t spk_fc_bias[];
+extern const int32_t spk_fc_mult[];
+extern const int32_t spk_fc_shift[];
+
+static const nmsis_nn_dims spk_fc_input_dims  = {1,1,1,SPK_FC_IN_DIM};
+static const nmsis_nn_dims spk_fc_filter_dims = {SPK_FC_OUT_DIM, 1, 1, SPK_FC_IN_DIM};
+static const nmsis_nn_dims spk_fc_bias_dims   = {SPK_FC_OUT_DIM, 1, 1, 1};
+static const nmsis_nn_dims spk_fc_out_dims    = {1, 1, 1, SPK_FC_OUT_DIM};
+
+static const nmsis_nn_fc_params spk_fc_params = {
+    .input_offset  = 128,
+    .filter_offset = 0,
+    .output_offset = 42,
+    .activation = {-128, 127}
+};
+
+static const nmsis_nn_per_channel_quant_params spk_fc_pc_quant = {
+    .multiplier = (int32_t *)spk_fc_mult,
+    .shift      = (int32_t *)spk_fc_shift
+};
---
+++ SPK/spk-OFFICIAL_ALL-NO_NOISE_hid512_7X12_epoch100_2cls/inc/spk_conv_model_params.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <stdint.h>
+#include "riscv_nnfunctions.h"
+
+#define SPK_CONV_IN_H      31
+#define SPK_CONV_IN_W      12
+#define SPK_CONV_IN_CH     1
+#define SPK_CONV_OUT_CH    52
+#define SPK_CONV_KH        7
+#define SPK_CONV_KW        12
+#define SPK_CONV_OUT_H     25
+#define SPK_CONV_OUT_W     1
+
+extern const int8_t  spk_conv_weights[];
+extern const int32_t spk_conv_bias[];
+extern const int32_t spk_conv_mult[];
+extern const int32_t spk_conv_shift[];
+
+static const nmsis_nn_dims spk_conv_input_dims  = {1, SPK_CONV_IN_H,  SPK_CONV_IN_W,  SPK_CONV_IN_CH};
+static const nmsis_nn_dims spk_conv_filter_dims = {SPK_CONV_OUT_CH,  SPK_CONV_KH,    SPK_CONV_KW,    SPK_CONV_IN_CH};
+static const nmsis_nn_dims spk_conv_bias_dims   = {SPK_CONV_OUT_CH,  1,          1,          1};
+static const nmsis_nn_dims spk_conv_out_dims    = {1, SPK_CONV_OUT_H, SPK_CONV_OUT_W, SPK_CONV_OUT_CH};
+
+static const nmsis_nn_conv_params spk_conv_params = {
+    .input_offset  = 0,
+    .output_offset = -128,
+    .stride        = {1,1},
+    .padding       = {0,0},
+    .dilation      = {1,1},
+    .activation    = {128,127}
+};
+
+static const nmsis_nn_per_channel_quant_params spk_conv_pc_quant = {
+    .multiplier = (int32_t*)spk_conv_mult,
+    .shift      = (int32_t*)spk_conv_shift
+};
